10140808b660896b33ce75eea31010ab3a24551d
mozesrahangmetan <mozesrahangmetan@mbp-van-mozes.home>
2016-09-23 17:06:44 +0200

    fix test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,16 +1,45 @@
 package com.example;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.example.controller.RootController;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(RootController.class)
 public class DemoApplicationTests {
+	 private MockMvc mvc;
+	 @Before
+	    public void setUp() throws Exception {
+	        mvc = MockMvcBuilders.standaloneSetup(
+	                new RootController())
+	            .build();
+	    }
 
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		
+		mvc.perform(MockMvcRequestBuilders
+	            .get("/about")
+	            .accept(MediaType.APPLICATION_JSON))
+	            .andExpect(status().isOk())
+	            .andExpect(content().string(
+	                is("About")));
 	}
 
 }
